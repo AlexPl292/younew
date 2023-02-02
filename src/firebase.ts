9f901639672fb3db5dc1a201bad3a0f4ca1e3c7f
Alex Plate <AlexPl292@gmail.com>
2023-02-02 20:19:01 +0200

    Attach firebase

---
+++ src/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+export const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCaCiRGHQWoGnquV9ouxNVhIhhrM2mNV2g",
+  authDomain: "younew-2c958.firebaseapp.com",
+  projectId: "younew-2c958",
+  storageBucket: "younew-2c958.appspot.com",
+  messagingSenderId: "15500001386",
+  appId: "1:15500001386:web:cc329b08eacbbadbcc4a13",
+  measurementId: "G-KJ8X6WKJVG",
+});
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
+import { VueFire } from "vuefire";
+import { firebaseApp } from "./firebase";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
+app.use(VueFire, {
+  // imported above but could also just be created here
+  firebaseApp,
+  modules: [
+    // we will see other modules later on
+  ],
+});
 
-app.mount('#app')
+app.mount("#app");
