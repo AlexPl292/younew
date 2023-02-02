81c2f009d563c5e44ff103441fce5dcd9b908af0
Alex Plate <AlexPl292@gmail.com>
2023-02-02 21:42:39 +0200

    Update presentation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,10 +18,13 @@ export default {
       let max = json.map((x) => x.population).reduce((a, b) => a + b, 0);
       let newPlace = getRandomArbitrary(0, max);
       let acc = 0;
-      let res;
+      let res = {};
       for (let item in json) {
         if (newPlace >= acc && newPlace < acc + json[item].population) {
-          res = json[item].country;
+          res.country = json[item].country;
+          res.perc = ((json[item].population / max) * 100)
+            .toFixed(20)
+            .match(/^-?\d*\.?0*\d{0,2}/)[0];
           break;
         }
         acc += json[item].population;
@@ -32,4 +35,11 @@ export default {
 };
 </script>
 
-<template>You are now: {{ hello }}</template>
+<template>
+  <h1>Assuming you have been reborn</h1>
+  <span>After a die roll you now live in</span>
+    <br>
+  <span style="font-size: 350%; background: inherit;">{{ hello.country }}</span>
+    <br>
+  <span>With other {{ hello.perc }}% like you</span>
+</template>
