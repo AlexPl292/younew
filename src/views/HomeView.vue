95b3af2b780a6c1ae503f9676f5f2aaed803403c
Alex Plate <AlexPl292@gmail.com>
2023-02-02 19:51:00 +0200

    Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<script>
+import json from "./data.json";
+
+function getRandomArbitrary(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default {
+  data() {
+    return {
+      count: json,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+  computed: {
+    hello() {
+      let max = json.map((x) => x.population).reduce((a, b) => a + b, 0);
+      let newPlace = getRandomArbitrary(0, max);
+      let acc = 0;
+      let res;
+      for (let item in json) {
+        if (newPlace >= acc && newPlace < acc + json[item].population) {
+          res = json[item].country;
+          break;
+        }
+        acc += json[item].population;
+      }
+      return res;
+    },
+  },
+};
+</script>
+
+<template>You are now: {{ hello }}</template>
