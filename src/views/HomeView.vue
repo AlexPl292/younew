835fdeda9d36eda263979793f3424af1ccf85858
Alex Plate <AlexPl292@gmail.com>
2023-02-03 16:44:58 +0200

    Add cities data caltulation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
-import json from "./data.json";
+import countries from "./data.json";
+import cities from "./cities.json";
 
 function getRandomArbitrary(min: number, max: number): number {
   min = Math.ceil(min);
@@ -8,26 +9,64 @@ function getRandomArbitrary(min: number, max: number): number {
 }
 
 export default {
-  data() {
-    return {
-      count: json,
-    };
-  },
   computed: {
     hello() {
-      let max = json.map((x) => x.population).reduce((a, b) => a + b, 0);
-      let newPlace = getRandomArbitrary(0, max);
+      let max = countries.map((x) => x.population).reduce((a, b) => a + b, 0);
+      let cities_dice = getRandomArbitrary(0, max);
       let acc = 0;
-      let res = {};
-      for (let item in json) {
-        if (newPlace >= acc && newPlace < acc + json[item].population) {
-          res.country = json[item].country;
-          res.perc = ((json[item].population / max) * 100)
+      let res = {
+        country: "",
+        country_code: "",
+        city: "",
+        city_local: "",
+        city_perc: "" as string | undefined,
+        perc: "" as string | undefined,
+        country_population: 0,
+      };
+      for (let item in countries) {
+        if (
+          cities_dice >= acc &&
+          cities_dice < acc + countries[item].population
+        ) {
+          res.country = countries[item].country;
+          res.country_code = countries[item].code;
+          res.perc = ((countries[item].population / max) * 100)
             .toFixed(20)
-            .match(/^-?\d*\.?0*\d{0,2}/)[0];
+            .match(/^-?\d*\.?0*\d{0,2}/)?.[0];
+          res.country_population = countries[item].population;
           break;
         }
-        acc += json[item].population;
+        acc += countries[item].population;
+      }
+
+      let big_cities_acc = 0;
+      for (let index in cities) {
+        if (cities[index].country_code == res.country_code) {
+          if (
+            cities_dice >= acc &&
+            cities_dice < acc + cities[index].population
+          ) {
+            res.city = cities[index].ascii_name;
+            res.city_local = cities[index].name;
+            res.city_perc = (
+              (cities[index].population / res.country_population) *
+              100
+            )
+              .toFixed(20)
+              .match(/^-?\d*\.?0*\d{0,2}/)?.[0];
+            break;
+          }
+          big_cities_acc += cities[index].population;
+          acc += cities[index].population;
+        }
+      }
+      if (!res.city) {
+        res.city_perc = (
+          ((res.country_population - big_cities_acc) / res.country_population) *
+          100
+        )
+          .toFixed(20)
+          .match(/^-?\d*\.?0*\d{0,2}/)?.[0];
       }
       return res;
     },
@@ -38,8 +77,17 @@ export default {
 <template>
   <h1>Assuming you have been reborn</h1>
   <span>After a die roll you now live in</span>
-    <br>
-  <span style="font-size: 350%; background: inherit;">{{ hello.country }}</span>
-    <br>
+  <br />
+  <span style="font-size: 350%; background: inherit">{{ hello.country }}</span>
+  <br />
   <span>With other {{ hello.perc }}% like you</span>
+  <br />
+  <span v-if="hello.city" style="font-size: 150%">
+    Your new city: {{ hello.city }} ({{ hello.city_local }})
+  </span>
+  <span v-else style="font-size: 150%">
+    You live in a city with less than 500 000 people
+  </span>
+  <br />
+  <span> Like {{ hello.city_perc }}% in this city </span>
 </template>
